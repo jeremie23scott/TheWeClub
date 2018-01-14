59dd5128db81597f4c538417c2fbecbc2804a8f6
Jatin Pasrija <jeremie23scott@gmail.com>
2018-01-14 23:03:04 +0530

    Test

--- app/src/main/java/com/theweclub/scotts/SplashActivity.java
+++ app/src/main/java/com/theweclub/scotts/SplashActivity.java
@@ -12,5 +12,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         avLoadingIndicatorView=findViewById(R.id.avi);
+        //Testing Commit
     }
 }
